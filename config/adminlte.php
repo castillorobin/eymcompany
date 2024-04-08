23b6bd7ab6dd81773b3c0462a0040bf1e5dfad41
castillorobin <robin.castillo@catolica.edu.sv>
2024-04-08 12:16:40 -0600

    cambios menu

--- config/adminlte.php
+++ config/adminlte.php
@@ -316,77 +316,48 @@ return [
             'can' => 'manage-blog',
         ],
         [
-            'text' => 'pages',
+            'text' => 'Personal',
             'url' => 'admin/pages',
             'icon' => 'far fa-fw fa-file',
-            'label' => 4,
+            
             'label_color' => 'success',
         ],
-        ['header' => 'account_settings'],
+        
         [
-            'text' => 'profile',
+            'text' => 'Compras',
+            'url' => 'admin/pages',
+            'icon' => 'far fa-fw fa-file',
+            
+            'label_color' => 'success',
+        ],
+
+        [
+            'text' => 'Proveedores',
+            'url' => 'admin/pages',
+            'icon' => 'far fa-fw fa-file',
+            
+            'label_color' => 'success',
+        ],
+        [
+            'text' => 'Inventario',
+            'url' => 'admin/pages',
+            'icon' => 'far fa-fw fa-file',
+            
+            'label_color' => 'success',
+        ],
+
+        ['header' => 'Configuraciones'],
+        [
+            'text' => 'Perfil',
             'url' => 'admin/settings',
             'icon' => 'fas fa-fw fa-user',
         ],
         [
-            'text' => 'change_password',
+            'text' => 'Cambiar Contraseña',
             'url' => 'admin/settings',
             'icon' => 'fas fa-fw fa-lock',
         ],
-        [
-            'text' => 'multilevel',
-            'icon' => 'fas fa-fw fa-share',
-            'submenu' => [
-                [
-                    'text' => 'level_one',
-                    'url' => '#',
-                ],
-                [
-                    'text' => 'level_one',
-                    'url' => '#',
-                    'submenu' => [
-                        [
-                            'text' => 'level_two',
-                            'url' => '#',
-                        ],
-                        [
-                            'text' => 'level_two',
-                            'url' => '#',
-                            'submenu' => [
-                                [
-                                    'text' => 'level_three',
-                                    'url' => '#',
-                                ],
-                                [
-                                    'text' => 'level_three',
-                                    'url' => '#',
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-                [
-                    'text' => 'level_one',
-                    'url' => '#',
-                ],
-            ],
-        ],
-        ['header' => 'labels'],
-        [
-            'text' => 'important',
-            'icon_color' => 'red',
-            'url' => '#',
-        ],
-        [
-            'text' => 'warning',
-            'icon_color' => 'yellow',
-            'url' => '#',
-        ],
-        [
-            'text' => 'information',
-            'icon_color' => 'cyan',
-            'url' => '#',
-        ],
+        
     ],
 
     /*
