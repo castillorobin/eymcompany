7b0f658d878b2a281087eec0754ab4ec3cbacee2
castillorobin <robin.castillo@catolica.edu.sv>
2024-06-12 16:00:17 -0600

    cambos en copras id

--- app/Http/Controllers/ComprasController.php
+++ app/Http/Controllers/ComprasController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers;
 
 
 use App\Models\Producto;
-use App\Models\Proveedor;
+use App\Models\Proveedor; 
 use App\Models\Compras;
 use App\Models\Detallecompra;
 use \PDF;
+use App\Models\Cardex;
 
 use Illuminate\Http\Request;
 
@@ -30,7 +31,8 @@ class ComprasController extends Controller
     {
         $proveedores = Proveedor::all();
         $detalles = Detallecompra::all();
-        return view('compra.crear', compact('proveedores', 'detalles'));
+        $productos = Producto::all();
+        return view('compra.crear', compact('proveedores', 'detalles', 'productos'));
     }
 
     public function ver($id)
@@ -57,7 +59,7 @@ class ComprasController extends Controller
     public function guardarenc(Request $request)
     {
         $proveedores = Proveedor::all();
-
+        $id = $request->get('producto');  
         $encabezado = new Compras();
 
         $encabezado->Fecha = $request->get('fecha');
@@ -73,14 +75,52 @@ class ComprasController extends Controller
  
        $linea = new Detallecompra();
        $linea->idcompra = $ultimoid->id;
+       $linea->idproducto = $id;
        $linea->descripcion = $request->get('detalle');
        $linea->cantidad = $request->get('cantidad');
        $linea->preciouni = $request->get('precio');
        $linea->subtotal = $request->get('subtotal');
        $linea->save();
+        
+     
+       $productos = Producto::all();
+
        $detalles = Detallecompra::where('idcompra', $idcompr)->get();
 
-        return view('compra.creardetalles', compact('proveedores', 'ultimoid', 'detalles'));
+        return view('compra.creardetalles', compact('proveedores', 'ultimoid', 'detalles', 'productos'));
+    }
+
+    public function guardartodo(Request $request)
+    {
+        //$proveedores = Proveedor::all();
+        $comprasl = $request->get('id');
+        $detalles = Detallecompra::where('idcompra', $comprasl)->get();
+
+        //$ultimoid = Compras::find($id);
+
+        $id = $request->get('producto'); 
+
+        foreach($detalles as $detalle)
+        {
+            $carde = new Cardex();
+            $carde->idproducto = $detalle->idproducto;
+            $carde->nota = $request->get('ccf');
+            $carde->tipo = "Carga";
+            $carde->Cantidad = $detalle->cantidad;
+            $carde->save();
+
+            $producto = Producto::find($detalle->idproducto);
+
+        $producto->Cantidad =  $producto->Cantidad + $detalle->cantidad; 
+        $producto->save();
+
+        }
+
+        
+
+    
+        $compras = Compras::all();
+        return view('compra.index', compact('compras'));
     }
 
     public function guardardet(Request $request)
@@ -88,10 +128,12 @@ class ComprasController extends Controller
         $proveedores = Proveedor::all();
         $comprasl = $request->get('id');
         $ultimoid = Compras::find($comprasl);
+        $id = $request->get('producto'); 
        //$idcompr = $ultimoid->id;
 
        $linea = new Detallecompra();
        $linea->idcompra = $request->get('id');
+       $linea->idproducto = $id;
        $linea->descripcion = $request->get('detalle');
        $linea->cantidad = $request->get('cantidad');
        $linea->preciouni = $request->get('precio');
@@ -99,7 +141,9 @@ class ComprasController extends Controller
        $linea->save();
        $detalles = Detallecompra::where('idcompra', $comprasl)->get();
 
-        return view('compra.creardetalles', compact('proveedores', 'ultimoid', 'detalles'));
+       $productos = Producto::all();
+
+        return view('compra.creardetalles', compact('proveedores', 'ultimoid', 'detalles', 'productos'));
 
 
     }
